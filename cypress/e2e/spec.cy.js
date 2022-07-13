3c5ca09796917b276e13ce13ce09be994ea2b50d
caroline birgersson <caroline@birgerochblom.se>
2022-07-13 11:05:57 +0200

    removed package

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Fan page', () => {
   it('should be accessible!', () => {
-    cy.visit("http://127.0.0.1:8081/code/")
+    cy.visit("http://127.0.0.1:8080/code/")
     cy.wait(500)
     cy.injectAxe()
     cy.checkA11y()
